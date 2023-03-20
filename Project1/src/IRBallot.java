996c0d39c3019428a6e00544eed278b4dbe08939
Robert Wang <robwang.us@gmail.com>
2023-03-19 21:17:58 -0500

    halfway done with IR

--- Project1/src/IRBallot.java
+++ Project1/src/IRBallot.java
@@ -1,7 +1,17 @@
 // inherits from abstract class Ballot
 // represents a single ballot in an IR election
 
+import java.util.PriorityQueue;
+
 // TODO:: actually implement
 public class IRBallot extends Ballot{
+    private PriorityQueue<IRCandidate> candidatesQueue;
 
+    public IRBallot(PriorityQueue<IRCandidate> candidatesQueue){
+        this.candidatesQueue = candidatesQueue;
+    }
+
+    public void redistributeVote(){
+        candidatesQueue.pop();
+    }
 }
