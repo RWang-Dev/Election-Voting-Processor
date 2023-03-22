d55ace8e94d59d43f8e715478b921695d0a7bc0d
Ethan-Loukusa <loukusaethan@gmail.com>
2023-03-22 00:04:06 -0500

    More testing

---
+++ Project1/testing/java/CPLFileProcessorTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CPLFileProcessorTest {
+    CPLFileProcessor processor = new CPLFileProcessor();
+
+
+    @Test
+    void CPLFileProcessorConstructorTest(){
+        assertNotNull(processor);
+    }
+
+    @Test
+    void processFileTest(){
+
+    }
+
+    @Test
+    void processBadFileTest(){
+
+    }
+
+}
