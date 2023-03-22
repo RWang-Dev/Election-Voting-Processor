6eb6810b05d2c271a16cd4e3158df3ea8aaf2b02
llamakking <gideon.tan@verizon.net>
2023-03-21 23:43:28 -0500

    Added documentation to some IR classes, and made some minor changes

--- Project1/src/IRCandidate.java
+++ Project1/src/IRCandidate.java
@@ -3,18 +3,37 @@
 
 import java.util.ArrayList;
 // TODO:: implement
+
+/**
+ * A candidate in an Instant Runoff election. IRCandidates are used in IRElections to determine the winner.
+ */
 public class IRCandidate extends Voteable{
     private ArrayList<Integer> voteCountHistory;
 
+    /**
+     * Creates a new IRCandidate instance with the given name and ID
+     * @param name The name of the candidate
+     * @param ID An int representing the unique ID of the party
+     */
     public IRCandidate(String name, int ID){
         this.name = name;
         this.ID = ID;
+        voteCountHistory = new ArrayList<Integer>();
     }
 
+    /**
+     * Updates the vote count history by adding the current number of votes to the history
+     */
     public void updateVoteCountHistory(){
 
         voteCountHistory.add(this.numVotes);
     }
+
+    /**
+     * Gets the vote count history
+     * @return an ArrayList representing the vote count history and recording the number of votes each
+     * time votes were redistributed after eliminating a candidate
+     */
     public ArrayList<Integer> getVoteCountHistory(){
 
         return this.voteCountHistory;
--- Project1/src/IRElection.java
+++ Project1/src/IRElection.java
@@ -2,28 +2,54 @@
 // represents a single IR election and conducts the necessary algorithms
 
 import java.util.ArrayList;
+
+/**
+ * An Instant Runoff election. An IRElection object is used to determine the winner in a given IR election.
+ */
 public class IRElection extends Election{
     private IRCandidate[] rankedCandidates;
     private ArrayList<IRCandidate> eliminatedCandidates;
 
+    /**
+     * Creates an instance of IRElection with a given list of rankedCandidates and list of ballots
+     * @param rankedCandidates a list of the candidates in the election, in order of the most votes to the least votes
+     * @param ballots a list of all the ballots in the election
+     */
     public IRElection(IRCandidate[] rankedCandidates, IRBallot[] ballots){
         this.rankedCandidates = rankedCandidates;
         this.ballots = ballots;
         this.eliminatedCandidates = new ArrayList<>();
     }
 
+    /**
+     * Gets the number of candidates in the election
+     * @return an int representing the number of candidates
+     */
     public int getNumCandidates(){
         return this.numVoteables;
     }
 
+    /**
+     * Gets the number of ballots in the election
+     * @return an int representing the number of ballots cast in the election
+     */
     public int getNumBallots(){
         return this.numBallots;
     }
 
+    /**
+     * Gets a list of the candidates in the election, in order of the most votes to the least votes
+     * @return a list of the candidates
+     */
     public Voteable[] getCandidates(){
         return this.rankedCandidates;
     }
 
+    /**
+     * Eliminates a candidate after no winner can be determined. The losing candidate is removed from the list
+     * of candidates and their votes are redistributed.
+     * @param cand the candidate to be eliminated
+     */
     public void eliminateCandidate(IRCandidate cand){
         this.eliminatedCandidates.add(cand);
 
@@ -44,21 +70,11 @@ public class IRElection extends Election{
         this.redistributeVotes(cand);
         this.reRankCandidates();
     }
-    public void reRankCandidates(){
-        int n = rankedCandidates.length;
-        for (int j = 1; j < n; j++) {
-            IRCandidate curr = rankedCandidates[j];
-            int key = rankedCandidates[j].numVotes;
-            int i = j - 1;
-            while ((i > -1) && (rankedCandidates[i].numVotes > key)) {
-                rankedCandidates[i + 1] = rankedCandidates[i];
-                i--;
-            }
-            rankedCandidates[i + 1] = curr;
-        }
-    }
 
-
+    /**
+     * Runs the Instant Runoff election. This involves eliminating a candidate until a candidate receives
+     * a majority of the votes, at which point that candidate is the winner of the election.
+     */
     public void runElection(){
         // Initial input for BallotHistory - for audit file
         for(IRCandidate tempCand : rankedCandidates) {
@@ -79,7 +95,11 @@ public class IRElection extends Election{
 
     }
 
-    public void redistributeVotes(IRCandidate candidate){
+    /**
+     * Redistributes an eliminated candidate's votes to the remaining candidates
+     * @param candidate the candidate to be eliminated
+     */
+    private void redistributeVotes(IRCandidate candidate){
         for(IRBallot ballot : (IRBallot[]) this.ballots){
             if(ballot.getCandidatesQueue().peek() == candidate){
                 ballot.redistributeVote();
@@ -93,9 +113,32 @@ public class IRElection extends Election{
         }
     }
 
+    /**
+     * Rerank the order of the candidates after eliminating one and redistributing their votes
+     */
+    private void reRankCandidates(){
+        int n = rankedCandidates.length;
+        for (int j = 1; j < n; j++) {
+            IRCandidate curr = rankedCandidates[j];
+            int key = rankedCandidates[j].numVotes;
+            int i = j - 1;
+            while ((i > -1) && (rankedCandidates[i].numVotes > key)) {
+                rankedCandidates[i + 1] = rankedCandidates[i];
+                i--;
+            }
+            rankedCandidates[i + 1] = curr;
+        }
+    }
+
+    /**
+     * Prints the election results to the screen
+     */
     public void printElectionResults(){
         System.out.println("The winner is: "+ rankedCandidates[0]);
     }
 
+    /**
+     * Produces an audit file in the same directory with the election information and election results
+     */
     public void produceAuditFile(){}
 }
