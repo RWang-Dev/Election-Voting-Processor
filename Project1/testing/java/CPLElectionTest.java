152476fe708e8777cdcb680b21c1760048589658
Ethan-Loukusa <loukusaethan@gmail.com>
2023-03-26 16:37:17 -0500

    Writing more tests

--- Project1/testing/java/CPLElectionTest.java
+++ Project1/testing/java/CPLElectionTest.java
@@ -7,11 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class CPLElectionTest {
     CPLFileProcessor processor = new CPLFileProcessor();
     CPLElection normal_election = (CPLElection) processor.processFile
-            ((new FileHandler("testCPL.csv")).openFile());
+            ((new FileHandler("Project1/testing/csvTestFiles/testCPL.csv")).openFile());
     CPLElection noBallotsElection = (CPLElection) processor.processFile
-            ((new FileHandler("testCPLNoBallots.csv")).openFile());
-    CPLElection change_winners_election = (CPLElection) processor.processFile
-            ((new FileHandler("testCPLChangeWinners.csv")).openFile());
+            ((new FileHandler("Project1/testing/csvTestFiles/testCPLNoBallots.csv")).openFile());
+    CPLElection twoTiedElection = (CPLElection) processor.processFile
+            ((new FileHandler("Project1/testing/csvTestFiles/testCPLTwoTie.csv")).openFile());
+    CPLElection threeTiedElection = (CPLElection) processor.processFile
+            ((new FileHandler("Project1/testing/csvTestFiles/testCPLThreeTie.csv")).openFile());
     @Test
     void clearArrayTest() {
     }
@@ -23,11 +25,16 @@ class CPLElectionTest {
     @Test
     void runElectionTest() {
         // Test this when we do system testing
+        normal_election.runElection();
+        noBallotsElection.runElection();
+        twoTiedElection.runElection();
+        threeTiedElection.runElection();
     }
 
     @Test
     void printElectionResultsTest() {
         // Test this when we do system testing
+        normal_election.runElection();
     }
 
     @Test
