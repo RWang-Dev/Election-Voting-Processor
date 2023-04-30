c591836c4e8e673467f8260b32c5f76a56412f98
PeachAuthor <alexiliarski@gmail.com>
2023-04-29 19:16:57 -0500

    POFileProcessorTest all good

--- Project2/src/POFileProcessorTest.java
+++ Project2/src/POFileProcessorTest.java
@@ -23,50 +23,34 @@ public class POFileProcessorTest {
 
     @Test
     void processFileWithNoBallotsTest(){
-//        FileHandler fh = new FileHandler("csvTestFiles/testIRNoBallots.csv");
-//        File[] no_ballot_files = new File[] {fh.openFile()} ;
-//        POElection election = (POElection) processor.processFile(no_ballot_files);
-//        assertNotNull(election);
-//        assertEquals(0, election.getNumBallots());
-//        assertEquals(4, election.getNumCandidates());
-//        for (Ballot ballot: election.getBallots()){
-//            assertNotNull(ballot);
-//            assertTrue(((IRBallot) ballot).getCandidatesQueue().size() > 0);
-//        }
-//        for (Voteable candidate: election.getCandidates()){
-//            assertNotNull(candidate);
-//            assertNotNull(candidate.getName());
-//        }
-    }
+        FileHandler fh = new FileHandler("testPONoBallots.csv");
+        File[] no_ballot_files = new File[] {fh.openFile()} ;
+        POElection election = (POElection) processor.processFile(no_ballot_files);
+        assertNotNull(election);
+        assertEquals(0, election.getNumBallots());
+        assertEquals(4, election.getNumVoteables());
 
-    // Tests processFile() with null input and with
-    // files that aren't found to ensure clean handling.
-    @Test
-    void processBadFileTest() {
-        File[] nonexistent_files = new File[] { (new FileHandler("Nonexistentfile.csv")).openFile() };
-        assertThrows(IllegalArgumentException.class,
-                () -> processor.processFile(nonexistent_files));
-        assertThrows(NullPointerException.class, ()->processor.processFile(null));
+        for (Voteable candidate: election.getVoteables()){
+            assertNotNull(candidate);
+            assertNotNull(candidate.getName());
+        }
     }
 
     // Test that multiple file functionality works
     @Test
     void processManyFilesTest(){
-//        File[] files = new File[] {(new FileHandler("testIR.csv")).openFile(),
-//                (new FileHandler("testIR2.csv")).openFile(),
-//                (new FileHandler("testIR3.csv")).openFile()} ;
-//        POElection election = (POElection) processor.processFile(files);
-//        assertNotNull(election);
-//        assertEquals(28, election.getNumBallots());
-//        assertEquals(4, election.getNumCandidates());
-//        for (Ballot ballot: election.getBallots()){
-//            assertNotNull(ballot);
-//            assertTrue(((IRBallot) ballot).getCandidatesQueue().size() > 0);
-//        }
-//        for (Voteable candidate: election.getCandidates()){
-//            assertNotNull(candidate);
-//            assertNotNull(candidate.getName());
-//        }
+        File[] files = new File[] {(new FileHandler("testPO.csv")).openFile(),
+                (new FileHandler("testPOAllTie.csv")).openFile(),
+                (new FileHandler("testPOTwoTie.csv")).openFile()} ;
+        POElection election = (POElection) processor.processFile(files);
+        assertNotNull(election);
+        assertEquals(64, election.getNumBallots());
+        assertEquals(6, election.getNumVoteables());
+
+        for (Voteable candidate: election.getVoteables()){
+            assertNotNull(candidate);
+            assertNotNull(candidate.getName());
+        }
 
     }
 }
